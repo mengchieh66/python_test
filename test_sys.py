e9a02a9c289d678653a268701d8e6ff4d5910b28
meng <u9924017@gmail.com>
2020-07-18 14:53:36 +0800

    add random number

--- test_sys.py
+++ test_sys.py
@@ -1,6 +1,19 @@
+'''
+python 3.x
+'''
+
 import sys
+import random
 
 name="meng"
 #print(sys.path)
 hello_format="hello ,{}"
 print(hello_format.format(name))
+
+#random a number
+generate_number=random.random()
+print("random a number with 2 decimals >>> {:.2f}".format(generate_number*100))
+#random a interger number
+generate_int= random.randint(1,99)
+print("random a interger >>> {}".format(generate_int))
+#
